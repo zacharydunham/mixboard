e27c2dcd71488cc946dc9a7176bf67cd6245a50d
Zach Dunham <zach.dunham@evanta.com>
2019-04-23 15:18:06 -0700

    Add user following

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -95,4 +95,22 @@ class UserTest < ActiveSupport::TestCase
     zach.unfollow(archer)
     assert_not zach.following?(archer)
   end
+
+  test "feed should have the right posts" do
+    zach = users(:zach)
+    archer  = users(:archer)
+    lana    = users(:lana)
+    # Posts from followed user
+    lana.microposts.each do |post_following|
+      assert zach.feed.include?(post_following)
+    end
+    # Posts from self
+    zach.microposts.each do |post_self|
+      assert zach.feed.include?(post_self)
+    end
+    # Posts from unfollowed user
+    archer.microposts.each do |post_unfollowed|
+      assert_not zach.feed.include?(post_unfollowed)
+    end
+  end
 end
