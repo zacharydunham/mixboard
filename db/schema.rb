1eade007a08886a715099bbb4731e1cac9336964
Zach Dunham <zach.dunham@gmail.com>
2019-06-30 19:31:58 -0700

    articles added to mixboard

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190418224924) do
+ActiveRecord::Schema.define(version: 20190628040349) do
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "microposts", force: :cascade do |t|
     t.text "content"
